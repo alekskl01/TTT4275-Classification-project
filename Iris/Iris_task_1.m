78feeb8b8ede2d784f7924cb9d18550f5f26eab3
alekskl01 <aleksander.klu@hotmail.com>
2023-04-20 14:09:24 +0200

    sgd

--- Iris/Iris_task_1.m
+++ Iris/Iris_task_1.m
@@ -2,9 +2,23 @@
 % By Sigurd von Brandis and Aleksander KLund
 
 %% Load the data
-class_Setosa = load('class_1');
-class_Versicolor = load('class_2');
-class_Virginica = load('class_3');
+class_Setosa_all = load('class_1','-ascii');
+class_Versicolor_all = load('class_2','-ascii');
+class_Virginica_all = load('class_3','-ascii');
+
+class_Setosa= [x1all(:,4) x1all(:,1) x1all(:,2)];
+class_Versicolor= [x2all(:,4) x2all(:,1) x2all(:,2)];
+class_Virginica= [x3all(:,4) x3all(:,1) x3all(:,2)];
+
+%class_Setosa= [x1all(:,3) x1all(:,4)];
+%class_Versicolor= [x2all(:,3) x2all(:,4)];
+%class_Virginica= [x3all(:,3) x3all(:,4)];
+
+% class_Setosa= [x1all(:,4)];
+% class_Versicolor= [x2all(:,4)];
+% class_Virginica= [x3all(:,4)];
+
+[Ntot,dimx] = size(x1);
 
 %% Make Training and test sets
 TrainingSetLength = 1:30;
@@ -23,26 +37,26 @@ Tot_Testing_Data = [class_Setosa(TestSetLength,:);
                     class_Versicolor(TestSetLength,:)];
 
 %% Make matrices used in confusion matrix
-Correct_Answer_Training = [kron(ones(1,N_Training),[1; 0; 0]);
-                           kron(ones(1,N_Training),[0; 1; 0]); 
-                           kron(ones(1,N_Training),[0; 0; 1])];
-
-Correct_Answer_Testing = [kron(ones(1,N_Testing),[1; 0; 0]);
-                          kron(ones(1,N_Testing),[0; 1; 0]); 
-                          kron(ones(1,N_Testing),[0; 0; 1])];
-
-Measured_Answer_Taining = zeros(size(Correct_Answer_Training));
-Measured_Answer_Testing = zeros(size(Correct_Answer_Testing));
+% Correct_Answer_Training = [kron(ones(1,N_Training),[1; 0; 0]);
+%                            kron(ones(1,N_Training),[0; 1; 0]); 
+%                            kron(ones(1,N_Training),[0; 0; 1])];
+% 
+% Correct_Answer_Testing = [kron(ones(1,N_Testing),[1; 0; 0]);
+%                           kron(ones(1,N_Testing),[0; 1; 0]); 
+%                           kron(ones(1,N_Testing),[0; 0; 1])];
+% 
+% Measured_Answer_Taining = zeros(size(Correct_Answer_Training));
+% Measured_Answer_Testing = zeros(size(Correct_Answer_Testing));
 
 %% Train linear classifier
-W = eye(3, 5);
+W = eye(3, dimx+1);
 Alpha = 0.01;
 while true
     
     gradientMSE = 0;
 
     for k = 1:3*N_Training
-         xk = [Tot_Training_Data(k,:)'; 1];
+         xk = [Tot_Training_Data(k,:)'; 1]
          gk = sigmoid(W*xk);
          tk = Correct_Answer_Training;
          gradientMSE = gradientMSE + 0.5*(gk-tk).'*(gk-tk);
