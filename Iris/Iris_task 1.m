b8a3bddb1e1c616549337a5d2050e2de69c6811e
Sigbra <sigurdvbrandis@gmail.com>
2023-04-20 11:25:28 +0200

    Started on implementing algorithm for training linear classifier.

--- Iris/Iris_task 1.m
+++ Iris/Iris_task 1.m
@@ -1,3 +1,6 @@
+%% Iris - Task 1
+% By Sigurd von Brandis and Aleksander KLund
+
 %% Load the data
 class_Setosa = load('class_1');
 class_Versicolor = load('class_2');
@@ -20,13 +23,35 @@ Tot_Testing_Data = [class_Setosa{TestSetLength,:};
                     class_Versicolor{TestSetLength,:}];
 
 %% Make matrices used in confusion matrix
-Correct_Answer_Training = ;
-Correct_Answer_Testing = ;
-Measured_Answer_training = ;
-Measured_Answer_Testing = ;
+Correct_Answer_Training = [kron(ones(1,N_Training),[1; 0; 0]),
+                           kron(ones(1,N_Training),[0; 1; 0]), 
+                           kron(ones(1,N_Training),[0; 0; 1])];
 
+Correct_Answer_Testing = [kron(ones(1,N_Testing),[1; 0; 0]),
+                          kron(ones(1,N_Testing),[0; 1; 0]), 
+                          kron(ones(1,N_Testing),[0; 0; 1])];;
+
+Measured_Answer_Taining = zeros(size(Correct_Answer_Training));
+Measured_Answer_Testing = zeros(size(Correct_Answer_Testing));
 
 %% Train linear classifier
+W = eye(3);
+Alpha = 0.01;
+while true {
+    
+    gradientMSE = 0
+    for k = ... {
+        W = W - Alpha*gradientMSE
+         xk = [...]
+         gk = sigmoid(W*xk);
+         tk = ...;
+         gradientMSE = gradientMSE + 0.5*(gk-tk).'*(gk-tk);
+    }
+    
+    if ... {
+        break
+    }
+}
 
 %% Testing linear Classifier
 
