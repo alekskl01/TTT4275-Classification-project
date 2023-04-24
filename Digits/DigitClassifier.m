1f334aff3edd47e29ca0787d6906e834eb274e8b
Sigbra <sigurdvbrandis@gmail.com>
2023-04-24 12:08:41 +0200

    confusion matrix

--- Digits/DigitClassifier.m
+++ Digits/DigitClassifier.m
@@ -7,6 +7,7 @@ targets = zeros(10, num_test);
 num_chuncks = 60;
 chunck_size = 60000/num_chuncks;
 
+%Classifying
 for k = 1:num_test
     targets(testlab(k), k) = 1;
     test_sample = testv(k,:);
@@ -15,20 +16,17 @@ for k = 1:num_test
         trainv_chunck = trainv((chunck_size*(i-1)+1):chunck_size*i,:);
         distances =  dist(trainv_chunck, test_sample);
         disp(size(distances))
-<<<<<<< HEAD
-        [Min_dist,chunck_index] = min(distances,[],1);
+        [Min_dist,local_index] = min(distances,[],1);
         global_index = chunck_size*i + local_index;
         closest_from_chuncks(i,:) = [Min_dist; global_index];
-=======
-        [~,Shortest_index] = min(distances,[],1);
-        Global_index = ...;
-        Estimated_guess_i = trainlab(Global_index);
-        outputs(Estimated_guess_i + 1, i) = 1;
->>>>>>> 8af423b67ce931a4c5b7a7156fe5f5d5f536e8d2
     end
     [~,I] = min(closest_from_chuncks(:,1),[],2);
     closest_distance_index = closest_from_chuncks(I,2);
     outputs(trainlab(closest_distance_index), k) = 1;
 end
 
+%Confusion matrix and error rate
+
+figure(1)
+plotconfusion(targets, outputs, 'Classification result');
 
