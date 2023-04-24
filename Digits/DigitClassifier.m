f209d627d3b2a2287316a9fc4becec35de5540e5
alekskl01 <aleksander.klu@hotmail.com>
2023-04-24 15:51:29 +0200

    fixes

--- Digits/DigitClassifier.m
+++ Digits/DigitClassifier.m
@@ -4,29 +4,22 @@
 outputs = zeros(10, num_test);
 targets = zeros(10, num_test);
 
-num_chuncks = 60;
-chunck_size = 60000/num_chuncks;
-
 %Classifying
+tic;
 for k = 1:num_test
     targets(testlab(k)+1, k) = 1;
     test_sample = testv(k,:);
     closest_from_chuncks = zeros(num_chuncks, 2);
-    for i = 1:num_chuncks
-        trainv_chunck = trainv((chunck_size*(i-1)+1):chunck_size*i,:);
-        distances =  dist(trainv_chunck, test_sample');
-        [Min_dist, local_index] = min(distances,[],1);
-        global_index = chunck_size*(i-1) + local_index;
-        closest_from_chuncks(i,:) = [Min_dist global_index];
-    end
-    [~,I] = min(closest_from_chuncks(:,1),[],1);
-    closest_distance_index = closest_from_chuncks(I,2);
+    distances =  dist(trainv, test_sample');
+    [~, closest_distance_index] = min(distances,[],1);
     outputs(trainlab(closest_distance_index)+1, k) = 1;
 
     if mod(k, 500) == 0
-        disp(k)
+        disp(k*100/num_test + "% done")
     end
 end
+tac
+
 save('saveOutputs.mat', "outputs")
 save('saveTargets.mat', "targets")
 
@@ -34,4 +27,3 @@ save('saveTargets.mat', "targets")
 
 figure(1)
 plotconfusion(targets, outputs, 'Classification result');
-
