a442b16b8a82449650033587ee944b8293623738
alekskl01 <aleksander.klu@hotmail.com>
2023-04-24 14:01:26 +0200

    finished classifier (kind of)

--- Digits/DigitClassifier.m
+++ Digits/DigitClassifier.m
@@ -8,14 +8,20 @@ num_chuncks = 60;
 chunck_size = 60000/num_chuncks;
 
 for k = 1:num_test
-    correct_class = testlab(k);
+    targets(testlab(k), k) = 1;
     test_sample = testv(k,:);
+    closest_from_chuncks = zeros(num_chuncks, 2);
     for i = 1:num_chuncks
         trainv_chunck = trainv((chunck_size*(i-1)+1):chunck_size*i,:);
         distances =  dist(trainv_chunck, test_sample);
         disp(size(distances))
-        [~,I] = min(distances,[],1);
-
-        
+        [Min_dist,chunck_index] = min(distances,[],1);
+        global_index = chunck_size*i + local_index;
+        closest_from_chuncks(i,:) = [Min_dist; global_index];
     end
+    [~,I] = min(closest_from_chuncks(:,1),[],2);
+    closest_distance_index = closest_from_chuncks(I,2);
+    outputs(trainlab(closest_distance_index), k) = 1;
 end
+
+
